718819920bbfd31a19f41848fbe6539f6ab35712
rkstlohchab <rkstlohchab@gmail.com>
2023-01-02 12:38:16 +0530

    practicecpp

--- DSAapnacllg/array/selectionsorting.cpp
+++ DSAapnacllg/array/selectionsorting.cpp
@@ -11,11 +11,19 @@ int main()
         cin>>arr[i];
     }
 
-    int max = INT_MIN;
+    for(int i=0; i<n-1;i++){
+        // int set_min_index = i;
+        for(int j =i+1; j<n;j++){
+            if (arr[i]>arr[j]){
+                // set_min_index = j;
+                swap(arr[i], arr[j]);
+            }
+        }
+        // swap(arr[set_min_index], arr[i]);
+    }
 
-    int count = 1;
-
-    for(int i=0; i<n-count; i++){
-    };
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
