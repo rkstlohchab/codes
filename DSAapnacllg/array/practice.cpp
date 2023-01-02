c0da5aeaadb279ced597bcc166f9c0ac9202d49f
rkstlohchab <rkstlohchab@gmail.com>
2023-01-02 12:37:04 +0530

    added practice.cpp

---
+++ DSAapnacllg/array/practice.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+// Most frequent number in an array
+/*
+int mostFrequent(int arr[], int n){
+    int maxcount=0;
+    int no_maxcnt;
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(int arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(count > maxcount){
+            maxcount = count;
+            no_maxcnt = arr[i];
+        }
+    }
+    return no_maxcnt;
+}
+
+int main()
+{
+    int arr[] = { 40,50,30,40,50,30,30 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << mostFrequent(arr, n);
+    return 0;
+}
+*/
+
+
+int main()
+{
+    
+    return 0;
+}
