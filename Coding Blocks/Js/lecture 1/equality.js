e8ab300202dda5c6c77c877e136a7a2119337b4e
rkstlohchab <rkstlohchab@gmail.com>
2023-01-22 11:10:49 +0530

    created js folder

---
+++ Coding Blocks/Js/lecture 1/equality.js
@@ -0,0 +1,30 @@
+// console.log(1==1);
+// console.log(1==2);
+
+// console.log(Infinity)
+// console.log(-Infinity)
+
+// console.log(3+(-3))
+
+// //Lose Equality
+// console.log(1=="1")
+// //Strict Equality
+// console.log(1==="1")
+
+// console.log('' == []) //true
+// console.log('' == 0) //true
+// console.log([] == 0) //true
+// console.log('\t' == 0) //true
+// console.log('\t' == '') //false because there is no type conversion as they are string
+
+
+
+//parseInt & parseFloat
+
+// let str = "123";
+// let num = parseInt(str);
+// console.log(num);
+
+// str = "abc123";
+// num = parseInt(str);
+// console.log(num);
---
+++ Coding Blocks/Js/lecture 1/functions.js
@@ -0,0 +1,20 @@
+//Functions method 1 
+// function helloWorld(){
+//     console.log("hello world");
+// }
+
+// helloWorld();
+
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(10,20))
+
+//Function method 2
+
+let sub = function (a,b) {
+    return a-b;
+}
+
+console.log(sub(100,20));
